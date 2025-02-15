1acd7f925b57fe2b1dbcadf4c58d643cc5239848
qkrwkd124 <qkrwkd120@naver.com>
2025-02-15 18:38:09 +0900

    sensor data 생성성

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,9 +1,9 @@
 import json
 import time
 import logging
+from typing import Dict,Any
 
-from confluent_kafka import Producer
-from kafka import KafkaProducer
+from confluent_kafka import Producer,Message
 from faker import Faker
 
 # 로깅 설정
@@ -15,16 +15,25 @@ logger = logging.getLogger(__name__)
 
 fake = Faker()
 
-conf = {'bootstrap.servers':'localhost:29092,localhost:29093,localhost:29094'}
-producer = KafkaProducer(
-    bootstrap_servers = ['localhost:29092','localhost:29093','localhost:29094'],
-    value_serializer = lambda v: json.dumps(v).encode('utf-8'),
-    acks='all',
-    retries=5,
-    linger_ms=5
-)
+conf:Dict[str,Any] = {
+    'bootstrap.servers':'localhost:29092',
+    'acks':'all',
+    'retries':5,
+    'linger.ms':5
+}
+
+
+# producer = KafkaProducer(
+#     bootstrap_servers = ['localhost:29092'],
+#     value_serializer = lambda v: json.dumps(v).encode('utf-8'),
+#     acks='all',
+#     retries=5,
+#     linger_ms=5
+# )
 # producer = Producer(conf)
-topic = 'sensor-data'
+
+producer:Producer = Producer(conf)
+topic:str = 'sensor-data'
 
 def generate_sensor_data():
     return {
@@ -33,16 +42,29 @@ def generate_sensor_data():
         "value": fake.pyfloat(min_value=20, max_value=30)
     }
 
+def delivery_report(err:Any,msg:Message)-> None:
+    if err is not None:
+        logger.error("Message delivery failed: %s", err)
+    else:
+         # 전송한 메시지의 value는 바이트형태이므로 다시 디코딩 후 JSON 로드
+        try :
+            msg_data = json.loads(msg.value().decode('utf-8'))
+        except Exception :
+            msg_data = msg.value()
+        logger.info("Message delivered to %s [%d] at offset %d",
+                    msg.topic(), msg.partition(), msg.offset())
+
 
 while True :
     data = generate_sensor_data()
     
     try :
-        future = producer.send(topic,value=data)
-        record_metadata = future.get(timeout=10)
-        logger.info("Sent: %s, topic: %s, partition: %d, offset: %d",
-                    data, record_metadata.topic, record_metadata.partition, record_metadata.offset)
+        # produce() 함수로 메시지를 전송합니다.
+        # value는 JSON 문자열로 변환한 후 UTF-8 인코딩 처리합니다.
+        producer.produce(topic,value=json.dumps(data).encode('utf-8'),callback=delivery_report)
+        # 메시지를 전송하고 버퍼에 넣습니다.
+        producer.poll(0)
     except Exception as e :
         logger.error("Failed to send message %s, error: %s", data, e)
     
-    time.sleep(1)
+    time.sleep(5) # 1초마다 메시지를 전송
